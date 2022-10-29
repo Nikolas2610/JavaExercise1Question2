5680dd9b20763bba79ed39844a775fdcbd2d327c
Nikolas2610 <npsillou@gmail.com>
2022-10-29 17:52:11 +0300

    Add logic for exercise

--- src/Question2.java
+++ src/Question2.java
@@ -2,17 +2,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.regex.Pattern;
 
 public class Question2 {
-
-    //C:\Java\Java22-23\Exercise1-Question2\data\movies.csv
-
     String line = "";
     String splitColumnBy = ",";
 
     ArrayList<Movie> movieList = new ArrayList<>();
-    int counter = 0;
+    ArrayList<Category> categoryList = new ArrayList<>();
+    ArrayList<Year> yearList = new ArrayList<>();
+    ArrayList<Word> wordList = new ArrayList<>();
+
+//    int counter = 0;
 
     {
         try {
@@ -20,12 +20,12 @@ public class Question2 {
             BufferedReader br = new BufferedReader(new FileReader("./data/movies.csv"));
             while ((line = br.readLine()) != null)   //returns a Boolean value
             {
-                if (counter > 1000) {
-                    System.out.println("Finish Reading");
-                    break;
-                } else {
-                    counter++;
-                }
+//                if (counter > 2000) {
+//                    System.out.println("Finish Reading");
+//                    break;
+//                } else {
+//                    counter++;
+//                }
                 String[] movie = line.split(splitColumnBy);    // use comma as separator
                 Movie m = new Movie(movie);
                 if (m.isCorrectRecord()) {
@@ -38,9 +38,105 @@ public class Question2 {
         }
 
 //        Print movies
+//        for (Movie movie : movieList) {
+//            movie.printMovie();
+//        }
+
+
         for (Movie movie : movieList) {
-            movie.printMovie();
+            //        Add categories List
+            ArrayList<String> tempCategories = movie.getCategories();
+            for (String tempCat : tempCategories) {
+                if (categoryList.isEmpty()) {
+                    categoryList.add(new Category(tempCat));
+                } else {
+                    boolean categoryExists = false;
+                    for (Category category : categoryList) {
+                        if (category.getName().equals(tempCat)) {
+                            category.increaseMovieCount();
+                            categoryExists = true;
+                            break;
+                        }
+                    }
+                    if (!categoryExists) {
+                        categoryList.add(new Category(tempCat));
+                    }
+                }
+            }
         }
+
+//        Add by year count
+        for (Movie movie : movieList) {
+            int tempYear = movie.getYear();
+            if (yearList.isEmpty()) {
+                yearList.add(new Year(tempYear));
+            } else {
+                boolean yearExists = false;
+                for (Year yearFromList : yearList) {
+                    if (yearFromList.getYear() == tempYear) {
+                        yearFromList.increaseYearCount();
+                        yearExists = true;
+                        break;
+                    }
+                }
+                if (!yearExists) {
+                    yearList.add(new Year(tempYear));
+                }
+            }
+        }
+
+
+        for (Movie movie : movieList) {
+            String[] wordsArray = movie.getWordsTitle();
+            for (String tempWord : wordsArray) {
+                if (wordList.isEmpty()) {
+                    wordList.add(new Word(tempWord));
+                } else {
+                    boolean wordExists = false;
+                    for (Word tempWordList : wordList) {
+                        if (tempWord.equals(tempWordList.getWord())) {
+                            wordExists = true;
+                            tempWordList.increaseWordCount();
+                            break;
+                        }
+                    }
+                    if (!wordExists) {
+                        wordList.add(new Word(tempWord));
+                    }
+                }
+            }
+        }
+
+        for (Category cat: categoryList) {
+            cat.printCategory();
+        }
+
+        for (Year year: yearList) {
+            year.printYear();
+        }
+
+
+        sortWordList();
+        int counterWords = 0;
+        for (Word word : wordList) {
+            word.printWord();
+            if (counterWords > 10) {
+                break;
+            } else {
+                counterWords++;
+            }
+        }
+
+
+    }
+
+    public void sortWordList() {
+        wordList.sort((d1, d2) -> {
+            if (d1.getWordCount() == d2.getWordCount()) {
+                return 0;
+            }
+            return d1.getWordCount() > d2.getWordCount() ? -1 : 1;
+        });
     }
 
 }
